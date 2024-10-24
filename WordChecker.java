6441fca4826fcba5a522205159bdb122adad15d7
socratest3 <socratest3@nycstudents.net>
2024-10-24 17:51:39 -0400

    Update WordChecker

---
+++ WordChecker.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+public class WordChecker {
+    private ArrayList<String> wordList;
+    public WordChecker(){
+        wordList = new ArrayList<String>();
+    }
+    public WordChecker(ArrayList<String> list)
+    {
+        wordList = list;
+    }
+    public boolean isWordChain() {
+        boolean wordChain = true;
+        for(int i = 1; i< wordList.size(); i++){
+            String after = wordList.get(i);
+            String before = wordList.get(i-1);
+        if(after.indexOf(before)< 0){
+            return false;
+        }
+    }
+     return wordChain;   
+    }
+
+    
+    public ArrayList<String> createList(String target){
+    ArrayList<String> list = new ArrayList<String>();
+    for(String s: wordList)
+    if(s.indexOf(target)==0)
+    list.add(s.substring(target.length()));
+    return list;
+}
+}
+ 
